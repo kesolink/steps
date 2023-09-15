25bc22f17884357393d4147c5f34c1a88dd2dba0
kesolink <kingleyumune14@gmail.com>
2023-09-15 19:30:17 +0100

    children update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ const messages = [
   "Invest your new income 🤑",
 ];
 export default function App() {
+  return(
+    <Steps />
+    
+  )
+}
+
+
+ function Steps() {
   const [step, setStep] = useState(1);
   // const [text, setText] = useState({name: "keso"});
   const [isOpen, setIsOpen] = useState(true);
@@ -27,19 +35,27 @@ export default function App() {
         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">step{step}: {messages[step - 1]} </p>
+      {/* <p className="message">step{step}: {messages[step - 1]} </p> */}
+      <StepMessage step={1}>
+      {messages[step - 1]}
+      </StepMessage>
+      
+
       <div className="buttons">
         <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>Previous</button>
         <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext} >Next</button>
       </div>
     </div>
-    )};
-    
-    
-
-    
+    )}
     </>
   );
 }
-
+function StepMessage({step, childern}){
+  return(
+    <div className="message">
+      <h3>step {step}</h3>
+      {childern}
+    </div>
+  )
+}
 
